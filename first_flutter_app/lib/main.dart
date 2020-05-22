de0f0830462da9e7ee5b6544bfa68764fe19102d
AlanMax26 <60798508+AlanMax26@users.noreply.github.com>
2020-05-22 05:58:42 -0500

    Initial commit

---
+++ first_flutter_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'main_content.dart';
+
+void main() => runApp(
+    MaterialApp(
+        title: "First App",
+        home: MainContent()
+        )
+    );
